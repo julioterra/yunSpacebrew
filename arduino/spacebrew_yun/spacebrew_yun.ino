3aa159ee76a81bcf5942a79ae6eac8b0d9860974
julio terra <julioterra@gmail.com>
2013-07-06 14:08:06 -0400

    Set-up the publish and subscribe methods and data types. Next up will work on sending data between to .ino and .py scripts using the console.

--- arduino/spacebrew_yun/spacebrew_yun.ino
+++ arduino/spacebrew_yun/spacebrew_yun.ino
@@ -13,18 +13,33 @@
  */
  
 
+struct Publisher {
+  char *name;
+  char *type;
+  char *defaultValue;
+  Publisher * next;
+};
+
+struct Subscriber{
+  char *name;
+  char *type;
+  Subscriber * next;
+};
+
 Process brew;
 String name;
 String server;
 int port;
 String description;
-String subscribers;
-String publishers;
+Subscriber * subscribers;
+Publisher * publishers;
 
+// make pid removing process into an object that is declared and instantiated 
+// in the set-up method so that these resources are relieved for later.
 Process pids;
 int const pidLength = 6;
-char pid [6] = {'\0','\0','\0','\0','\0','\0'};
 int const sbPidsLen = 4;
+char pid [6] = {'\0','\0','\0','\0','\0','\0'};
 int sbPids [4] = {-1, -1, -1, -1};
 
 
@@ -40,6 +55,10 @@ void setup() {
 	Serial.println("Bridge Started"); 
 
 	constructorSB();
+	addPublish("please", "boolean");
+	addPublish("work", "boolean");
+	addSubscribe("it's", "boolean");
+	addSubscribe("working", "boolean");
 	connectSB(); 
    
 //	Console.buffer(64);
@@ -51,20 +70,119 @@ void constructorSB() {
 	server = "sandbox.spacebrew.cc";
 	port = 9000;
 	description = "no description";
-	subscribers = "";
-	publishers = "";
 }
 
-//void addPublish(String name, String type) {
-//	publishers += name + "," + type + ";";
-//}
-//
-//void addSubscribe(String name, String type) {
-//	subscribers += name + "," + type + ";";
-//}
+void addPublish(const String name, const String type) {
+	Publisher *p = new Publisher();
+	p->name = createString(name.length() + 1);
+	p->type = createString(type.length() + 1);
+	name.toCharArray(p->name, name.length() + 1);
+	type.toCharArray(p->type, type.length() + 1);
 
-// 
+	if (publishers == NULL){
+		publishers = p;
+	} else {
+		Publisher *curr = publishers;
+		while(curr->next != NULL){
+			curr = curr->next;
+		}
+		curr->next = p;
+	}
+}
 
+void addPublish(char * name, char * type) {
+	addPublish( String(name), String(type) );
+}
+
+void addSubscribe(const String name, const String type) {
+	// subscriber += name + "," + type + ";";
+	Subscriber *p = new Subscriber();
+	p->name = createString(name.length() + 1);
+	p->type = createString(type.length() + 1);
+	name.toCharArray(p->name, name.length() + 1);
+	type.toCharArray(p->type, type.length() + 1);
+
+	if (subscribers == NULL){
+		subscribers = p;
+	} else {
+		Subscriber *curr = subscribers;
+		while(curr->next != NULL){
+			curr = curr->next;
+		}
+		curr->next = p;
+	}
+}
+
+void addSubscribe(char * name, char * type) {
+	addSubscribe( String(name), String(type) );
+}
+
+void connectSB() {
+
+	killBrewPids();
+
+ 	brew.begin("python"); // Process should launch the "curl" command
+	brew.addParameter("/usr/lib/python2.7/spacebrew.py"); // Process should launch the "curl" command
+	brew.addParameter("--server");
+	brew.addParameter(server);
+	brew.addParameter("--port");
+	brew.addParameter(String(port));
+	brew.addParameter("-n");
+	brew.addParameter(name);
+	brew.addParameter("-d");
+	brew.addParameter(description);
+
+	if (subscribers != NULL) {
+		Subscriber *curr = subscribers;
+		while(curr != NULL){
+			Serial.print("sub name: ");
+			Serial.println(curr->name);
+
+			brew.addParameter("-s"); // Add the URL parameter to "curl"
+			brew.addParameter(curr->name); // Add the URL parameter to "curl"
+			brew.addParameter(","); // Add the URL parameter to "curl"
+			brew.addParameter(curr->type); // Add the URL parameter to "curl"
+
+			if (curr->next == NULL) curr = NULL;
+			else curr = curr->next;
+		}
+	}
+	if (publishers != NULL) {
+		Publisher *curr = publishers;
+		while(curr != NULL){
+			Serial.print("pub name: ");
+			Serial.println(curr->name);
+
+			brew.addParameter("-p"); // Add the URL parameter to "curl"
+			brew.addParameter(curr->name); // Add the URL parameter to "curl"
+			brew.addParameter(","); // Add the URL parameter to "curl"
+			brew.addParameter(curr->type); // Add the URL parameter to "curl"
+
+			if (curr->next == NULL) curr = NULL;
+			else curr = curr->next;
+		}
+	}
+
+    Serial.println("connectSB - starting console");
+
+	Console.begin();
+	delay(500);
+
+	brew.run();                   // Run the process and wait for its termination	
+
+	while (!Console) { ; }        
+
+    Serial.println("connectSB - running spacebrew.py script");
+}
+
+
+void loop() { 
+} 
+
+
+/**
+ * method that gets the pid of all spacebrew.py instances running on the linino.
+ */
 void getSbPid() {
 
 	// request the pid of all python processes
@@ -107,6 +225,10 @@ void getSbPid() {
 	}
 }
 
+/**
+ * method that kills all of the spacebrew.py instances that are running 
+ * on the linino.
+ */
 void killBrewPids() {
 	getSbPid();
 	delay(400);
@@ -128,36 +250,15 @@ void killBrewPids() {
 	}
 }
 
-void connectSB() {
-
-	killBrewPids();
-
- 	brew.begin("python"); // Process should launch the "curl" command
-	brew.addParameter("/usr/lib/python2.7/spacebrew.py"); // Process should launch the "curl" command
-	brew.addParameter("--server");
-	brew.addParameter(server);
-	brew.addParameter("--port");
-	brew.addParameter(String(port));
-	brew.addParameter("-n");
-	brew.addParameter(name);
-	brew.addParameter("-d");
-	brew.addParameter(description);
-	brew.addParameter("-p"); // Add the URL parameter to "curl"
-	brew.addParameter("tested, range"); // Add the URL parameter to "curl"
-	brew.addParameter("-s"); // Add the URL parameter to "curl"
-	brew.addParameter("test, range"); // Add the URL parameter to "curl"
-
-    Serial.println("connectSB - command ready");
-
-	Console.begin();
-	delay(500);
-
-	brew.run();                   // Run the process and wait for its termination	
-
-    Serial.println("connectSB - check console");
-	while (!Console) { ; }        
+static char *cloneString(char *s){
+	int n = strlen(s);
+	char *out = (char *)malloc(n+1);//new char[n];
+	strcpy(out, s);
+	return out;
 }
 
+static char *createString(int n){
+	char *out = (char *)malloc(n+1);//new char[n];
+	return out;
+}
 
-void loop() { 
-} 
